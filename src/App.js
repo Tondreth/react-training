592143e899f56ef5b276ea48a509c1299eebe8f8
Nikolay Nikolov <nikolay.veselinov.nikolov@gmail.com>
2021-03-22 13:54:05 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { Component } from "react";
+import { CardList } from "./components/card-list/card-list.component"
+import { SearchBox } from "./components/search-box/search-box.component"
+import './App.css';
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ""
+    }
+  }
+
+  componentDidMount() {
+    fetch("http://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
+  handleChange = (event) => {
+    this.setState({ searchField: event.target.value });
+  }
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMosnters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
+    return (
+      <div className="App">
+        <h1>The Oof App</h1>
+        <SearchBox placeholder="search monsters..." handleChange={this.handleChange} />
+        <CardList monsters={filteredMosnters} />
+      </div>
+    )
+  }
+}
+
+export default App;
